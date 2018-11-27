2001f82e060e411970e524743a93e525972d9860
polydev <poly.dev2018@gmail.com>
2018-11-27 16:33:39 +0700

    modify body react again

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className='App'>
         <header className='App-header'>
           <img src={logo} className='App-logo' alt='logo' />
-        </header>
-        <p>
-          Deploy React App to Netlify Hosting
+          <p>
+            Deploy React App to Netlify Hosting
         </p>
+        </header>
       </div>
     )
   }
